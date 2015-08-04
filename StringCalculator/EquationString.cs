63250ad4e1681e492208f063caa8fae7bc0c7877
typheus <typheus.orikoma@gmail.com>
2015-08-04 15:42:25 -0400

    First save to get on cloud

---
+++ StringCalculator/EquationString.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator
+{
+    class EquationString
+    {
+        List<string> parsedEquation;
+
+
+        public EquationString()
+        {
+            parsedEquation = new List<string>();
+        }
+
+        public void ParseAndStoreEquation(string unparsedEquationString)
+        {
+
+        }
+    }
+}
---
+++ StringCalculator/Operations.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator
+{
+    class Operations
+    {
+        Dictionary<string, Func<double, double, double>> operationDictionary;
+
+        public Operations()
+        {
+            operationDictionary = new Dictionary<string,Func<double,double,double>>
+            {
+                {"+", Add},
+                {"-", Subtract},
+                {"*", Multiply},
+                {"/", Divide},                
+            };
+        }
+
+        private double Add(double firstNumber, double secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+
+        private double Subtract(double firstNumber, double secondNumber)
+        {
+            return firstNumber - secondNumber;
+        }
+
+        private double Multiply(double firstNumber, double secondNumber)
+        {
+            return firstNumber * secondNumber;
+        }
+
+        private double Divide(double firstNumber, double secondNumber)
+        {
+            return firstNumber / secondNumber;
+        }
+    }
+}
