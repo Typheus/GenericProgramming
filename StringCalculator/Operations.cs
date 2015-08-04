9668a486bc2fa8c20e7f0566213971e010ca5c21
typheus <typheus.orikoma@gmail.com>
2015-08-04 16:15:45 -0400

    Attempt 2

--- StringCalculator/Operations.cs
+++ StringCalculator/Operations.cs
@@ -29,8 +29,8 @@ namespace StringCalculator
 
         private double Subtract(double firstNumber, double secondNumber)
         {
-            return firstNumber - secondNumber;
 
+            return firstNumber - secondNumber;
         }
 
         private double Multiply(double firstNumber, double secondNumber)
